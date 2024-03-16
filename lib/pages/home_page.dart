719d8f8bae19346af32c497c7c6c909a4c7f2353
nahmed117 <nahmed117m@gmail.com>
2024-03-16 15:57:53 +0600

    version 1

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _State();
+}
+
+class _State extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
